665f1e52fcd316ebbd8babf2bc906bc2a83ab4d5
Vitali-Leikin <leikin1.19@gmail.com>
2021-06-13 18:49:28 +0300

    add task 1 and task 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,118 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        let counter = 0
+        var pricesArray = prices
+        var result = 0
+        var count = 0
+        var  temp = 0
+        
+        
+        
+        while counter < pricesArray.count - 1 {
+            
+            print(pricesArray)
+            
+            if pricesArray[counter] == pricesArray[counter + 1]{
+                
+                
+                
+                while pricesArray[counter] < pricesArray.max() ?? 0 {
+                    
+                    temp += pricesArray[counter]
+                    count += 1
+                    print("temp = \(temp)")
+                    print("count = \(count)")
+                    
+                    pricesArray.remove(at: counter)
+                    
+                }
+                let maxValueInArray = pricesArray.max() ?? 0
+                
+                if (result < (maxValueInArray * count  - temp)){
+                    result = maxValueInArray * count  - temp
+                }
+                print("result = \(result)")
+                
+                pricesArray.remove(at: counter)
+            }
+            
+            if pricesArray == [] || pricesArray.count == 1 {
+                return result
+            }
+            
+            
+            if (pricesArray[counter + 1] > pricesArray[counter]) {
+                
+                if (pricesArray.last == (pricesArray.max() ?? 0)) && pricesArray.count > 2{
+                    
+                    while pricesArray[counter] < pricesArray.max() ?? 0 {
+                        
+                        if (pricesArray[counter] == pricesArray.max() ?? 0 ) {
+                            pricesArray.remove(at: counter)
+                        }
+                        
+                        temp += pricesArray[counter]
+                        count += 1
+                        
+                        print("tempL = \(temp)")
+                        print("countL = \(count)")
+                        pricesArray.remove(at: counter)
+                        
+                        print(pricesArray)
+                        
+                    }
+                    
+                    let temz = pricesArray.max() ?? 0
+                    
+                    if (result < (temz * count  - temp)){
+                        result = temz * count  - temp
+                    }
+                    
+                    pricesArray.remove(at: counter)
+                    
+                }
+                
+                
+                if pricesArray.count > 1 {
+                    
+                    result += pricesArray[counter + 1] - pricesArray[counter]
+                    
+                    pricesArray.remove(at: counter)
+                    
+                    pricesArray.remove(at: counter)
+                    
+                }
+                
+            }
+            
+            
+            if pricesArray == []{
+                return result
+            }
+            
+            
+            if pricesArray.count > 1{
+                
+                if pricesArray[counter] > pricesArray[counter + 1]{
+                    
+                    pricesArray.remove(at: counter)
+                    
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+        if result < 0{
+            result = 0
+        }
+        
+        return result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,213 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
+    var countArray:[Int] = []
+    var tempArrayText:[String] = []
+        // var messageArray = ""
+    var resultSrtring = ""
     
     func decryptMessage(_ message: String) -> String {
-        return ""
-    }
+    
+   
+        calcWords(message)
+        
+        print(countArray)
+        print(tempArrayText)
+        
+        if countArray.count == 0 {
+            resultSrtring = tempArrayText.first ?? ""
+        }
+        
+        for (index, value) in countArray.enumerated() {
+            print(index)
+            print(value)
+            var counter = 0
+            
+            while counter < value {
+                resultSrtring.append(tempArrayText[index])
+                print(resultSrtring)
+                counter += 1
+            }
+        }
+        
+       
+
+        return resultSrtring
 }
+
+    
+    
+    func calcWords(_ message: String){
+        
+        
+        var messageArray = message
+        
+       print(messageArray)
+        
+        let patternOne = "^[a-z]{1,300}"
+    
+        do {
+            let regex = try NSRegularExpression(pattern: patternOne)
+            
+            if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                let partOne = messageArray[Range(match.range, in: messageArray)!]
+
+                print(String(partOne))
+                
+                print(String(messageArray))
+                tempArrayText.append(String(partOne))
+
+                print(tempArrayText)
+                
+                print(messageArray.removeFirst(partOne.count) )
+                
+                print(messageArray)
+            
+                countArray.append(1)
+
+                
+            } else{
+                
+                
+                let patternNumber = "[0-9]{1,300}"
+                do {
+                    let regex = try NSRegularExpression(pattern: patternNumber)
+
+                    if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                        let partN = messageArray[Range(match.range, in: messageArray)!]
+
+                        print(String(partN))
+
+                        countArray.append(Int(partN) ?? 0)
+
+
+                        print(messageArray.remove(at: partN.startIndex))
+                    }
+                } catch { print(error) }
+                
+                
+                
+            }
+        } catch { print(error) }
+    
+       
+            
+        let patternNumber = "\\b[0-9]{1,300}"
+   //     let patternNumber = "\\w.[0-9]{1,300}"
+        do {
+            let regex = try NSRegularExpression(pattern: patternNumber)
+
+            if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                let partN = messageArray[Range(match.range, in: messageArray)!]
+
+                print(String(partN))
+
+                countArray.append(Int(partN) ?? 0)
+
+
+                print(messageArray.remove(at: partN.startIndex))
+                
+            }
+//            else if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+//
+//                let partN = messageArray[Range(match.range, in: messageArray)!]
+//
+//                print(String(partN))
+//
+//                countArray.append(Int(partN) ?? 0)
+//
+//
+//                print(messageArray.remove(at: partN.startIndex))
+//
+//            }
+            
+        } catch { print(error) }
+        
+        
+        
+        
+        let patternTemp = "\\[|\\]"
+        do {
+            let regex = try NSRegularExpression(pattern: patternTemp)
+
+            if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                let partQ = messageArray[Range(match.range, in: messageArray)!]
+
+                print(String(partQ))
+
+                print(messageArray.remove(at: partQ.startIndex))
+            }
+        } catch { print(error) }
+        
+        
+       
+        let pattern = "[a-z]{1,300}"
+    
+        do {
+            let regex = try NSRegularExpression(pattern: pattern)
+            
+            if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                let part1 = messageArray[Range(match.range, in: messageArray)!]
+
+                print(String(part1))
+                
+                print(String(messageArray))
+                tempArrayText.append(String(part1))
+
+                print(tempArrayText)
+                
+                print(messageArray.removeFirst(part1.count) )
+                
+                print(messageArray)
+            
+                
+            }
+        } catch { print(error) }
+        
+        
+        let patternTempTwo = "\\[|\\]"
+        do {
+            let regex = try NSRegularExpression(pattern: patternTempTwo)
+
+            if let match = regex.firstMatch(in: messageArray, range: NSRange(messageArray.startIndex..., in: messageArray)) {
+
+                let partQTwo = messageArray[Range(match.range, in: messageArray)!]
+
+                print(String(partQTwo))
+
+                print(messageArray.remove(at: partQTwo.startIndex))
+            }
+        } catch { print(error) }
+        
+        
+    
+    if messageArray.count > 1 {
+        calcWords(messageArray)
+    }else{
+        
+        
+        print("result")
+    }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+
